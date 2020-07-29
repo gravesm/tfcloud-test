f135aec112111c3a0039d375cf700dba1a168869
Mike Graves <mgraves@mit.edu>
2020-07-29 11:37:19 -0400

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "remote" {
+    organization = "gravesmj"
+    workspaces {
+      prefix = "mgraves-"
+    }
+  }
+}
+
 provider "aws" {
   version = "~> 2.0"
   region  = "us-east-1"
