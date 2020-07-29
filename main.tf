d0873bec7580ed65d80701c7bf453544ed1704ef
Mike Graves <mgraves@mit.edu>
2020-07-29 09:57:46 -0400

    wip

--- main.tf
+++ main.tf
@@ -3,18 +3,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-terraform {
-  required_version = ">= 0.12"
-
-  backend "s3" {
-    region         = "us-east-1"
-    bucket         = "mit-tfstates-state"
-    key            = "apps/tfcloud.tfstate"
-    dynamodb_table = "mit-tfstates-state-lock"
-    encrypt        = true
-  }
-}
-
 resource "aws_iam_user" "default" {
-  name = "tf-cloud-${var.workspace}"
+  name        = "tf-cloud-${var.workspace}"
+  description = "Testy McTesterson"
 }
